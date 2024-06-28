0ce245916f186c131599493cddb6d2e275f895d9
Leonardo <leonardorscarpitta@gmail.com>
2024-06-28 14:03:13 -0300

    style: changing background to white

--- src/components/loginCard/loginCard.tsx
+++ src/components/loginCard/loginCard.tsx
@@ -7,7 +7,7 @@ interface LoginCardProps {
 
 export default function LoginCard({ title, children }: LoginCardProps ) {
   return (
-    <div className="flex flex-col gap-y-6 bg-white font-bold text-black rounded-lg p-16">
+    <div className="flex flex-col gap-y-6 bg-white font-bold text-black rounded-lg p-16 bg-white">
       <h1 className="text-center font-black text-purple-500 uppercase">{title}</h1>
       {children}
     </div>
